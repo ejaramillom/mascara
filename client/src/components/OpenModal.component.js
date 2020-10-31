57a197c75df52b804351d98ca30bb9e26fe9ed5d
Daniel <danielrs9504@gmail.com>
2020-10-31 13:38:13 -0500

    add axios request

--- client/src/components/OpenModal.component.js
+++ client/src/components/OpenModal.component.js
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Modal from 'react-bulma-components/lib/components/modal';
-import Button from 'react-bulma-components/lib/components/button';
-import Content from 'react-bulma-components/lib/components/content';
-import Section from 'react-bulma-components/lib/components/section';
-import { select, boolean } from '@storybook/addon-knobs';
-import { getBrushes, getBottles, getRods, getWipers, getCaps } from "../middlewares/services";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Modal from "react-bulma-components/lib/components/modal";
+import Button from "react-bulma-components/lib/components/button";
+import Content from "react-bulma-components/lib/components/content";
+import Section from "react-bulma-components/lib/components/section";
+import { select, boolean } from "@storybook/addon-knobs";
+import axios from "axios";
+
+import {
+  getBrushes,
+  getBottles,
+  getRods,
+  getWipers,
+  getCaps,
+} from "../middlewares/services";
 import { useQuery, useMutation } from "react-query";
-import { colors } from "../features/colors.js"
-import '../App.css';
+import { colors } from "../features/colors.js";
+import "../App.css";
 
 export class OpenModal extends React.Component {
   static propTypes = {
     modal: PropTypes.object,
     children: PropTypes.node.isRequired,
-    name: PropTypes.string
-  }
+    name: PropTypes.string,
+  };
 
   static defaultProps = {
     modal: {},
-    name: "name"
-  }
+    name: "name",
+  };
 
   state = {
     show: false,
-  }
+  };
 
   open = () => this.setState({ show: true });
   close = () => this.setState({ show: false });
@@ -32,9 +40,16 @@ export class OpenModal extends React.Component {
   render() {
     return (
       <div>
-        <Button color="info" onClick={this.open}> {this.props.name} </Button>
-        <br/>
-        <Modal show={this.state.show} onClose={this.close} {...this.props.modal}>
+        <Button color="info" onClick={this.open}>
+          {" "}
+          {this.props.name}{" "}
+        </Button>
+        <br />
+        <Modal
+          show={this.state.show}
+          onClose={this.close}
+          {...this.props.modal}
+        >
           {this.props.children}
         </Modal>
       </div>
@@ -43,70 +58,62 @@ export class OpenModal extends React.Component {
 }
 
 export const BottleModal = () => {
-  const addBottle = ({ bottleName }) => {
-    fetch( "/bottle", {
-      method: "POST",
-      body: JSON.stringify( bottleName ),
-    })
-    .then( res => {
-      if ( res.status === 200 ){
-        alert( "Bottle added to the list!" );
-        console.log( "Succesfully added" );
-      } else {
-        const err = new Error( res.error );
-        console.log( err );
-        throw err;
-      }
-    })
-    .catch( err => {
-      console.error( err );
-      alert( "Error saving bottle. Please, try again!" )
-    })
-  }
-  const [bottleName, setBottleName] = useState('');
-  const [mutate] = useMutation(addBottle)
+  const addMovie = async (data) => {
+    const ServerCall = await axios
+      .post("/bottle", {
+        bottleName: data.name,
+      })
+      .then(function (response) {
+        // console.log(response);
+        if (response.status === 200) {
+          alert("Bottle added to the list!");
+          console.log("Succesfully added");
+        } else {
+          const err = new Error(response.error);
+          console.log(err);
+          throw err;
+        }
+      })
+      .catch(function (error) {
+        alert(error);
+      });
+  };
   const { isLoading, error, data } = useQuery("bottles", getBottles);
   if (isLoading) return "Loading...";
   if (error) {
     return "Oops! " + error.message;
   }
 
-  const onAddBottle = async e => {
-    e.preventDefault()
-    try {
-      await mutate({ bottleName })
-    } catch (error) {
-    }
-  }
-
   return (
-  <div>
-    <OpenModal modal={{ closeOnBlur: true }} name="Bottle">
-      <div className="modal-body">
-        { data.map( element =>
-          <form onSubmit = { onAddBottle } >
-          <Modal.Content>
-            <Section style={{ backgroundColor: 'white' }}>
-            <p>
-              <strong>{element.name }</strong> <small>{element.drawing}</small> <small>{element.mold}</small>
-              <br />
-              Something else
-            </p>
-              <Button
-                type="submit"
-                color="info"
-                onClick={ () => {setBottleName(element.name)}}>
-                Add
-              </Button>
-            </Section>
-          </Modal.Content>
-          </form>
-        )}
-      </div>
-    </OpenModal>
-  </div>
+    <div>
+      <OpenModal modal={{ closeOnBlur: true }} name="Bottle">
+        <div className="modal-body">
+          {data.map((element) => (
+            <Modal.Content>
+              <Section style={{ backgroundColor: "white" }}>
+                <p>
+                  <strong>{element.name}</strong>{" "}
+                  <small>{element.drawing}</small> <small>{element.mold}</small>
+                  <br />
+                  Prueba de modal
+                </p>
+                <Button
+                  type="submit"
+                  color="info"
+                  onClick={() => {
+                    addMovie(element);
+                  }}
+                >
+                  Add
+                </Button>
+              </Section>
+            </Modal.Content>
+          ))}
+        </div>
+      </OpenModal>
+    </div>
   );
-}
+};
 
 export const BrushModal = () => {
   const { isLoading, error, data } = useQuery("brushes", getBrushes);
@@ -118,22 +125,24 @@ export const BrushModal = () => {
     <div>
       <OpenModal modal={{ closeOnBlur: true }} name="Brush">
         <div className="modal-body">
-          { data.map( element =>
+          <h1>hola</h1>
+          {data.map((element) => (
             <Modal.Content>
-              <Section style={{ backgroundColor: 'white' }}>
-              <p>
-                <strong>{element.name }</strong> <small>{element.drawing}</small> <small>{element.mold}</small>
-                <br />
-                Something else
-              </p>
+              <Section style={{ backgroundColor: "white" }}>
+                <p>
+                  <strong>{element.name}</strong>{" "}
+                  <small>{element.drawing}</small> <small>{element.mold}</small>
+                  <br />
+                  Something else
+                </p>
               </Section>
             </Modal.Content>
-          )}
+          ))}
         </div>
       </OpenModal>
     </div>
   );
-}
+};
 
 export const RodModal = () => {
   const { isLoading, error, data } = useQuery("rods", getRods);
@@ -145,22 +154,23 @@ export const RodModal = () => {
     <div>
       <OpenModal modal={{ closeOnBlur: true }} name="Rod">
         <div className="modal-body">
-          { data.map( element =>
+          {data.map((element) => (
             <Modal.Content>
-              <Section style={{ backgroundColor: 'white' }}>
-              <p>
-                <strong>{element.name }</strong> <small>{element.drawing}</small> <small>{element.mold}</small>
-                <br />
-                Something else
-              </p>
+              <Section style={{ backgroundColor: "white" }}>
+                <p>
+                  <strong>{element.name}</strong>{" "}
+                  <small>{element.drawing}</small> <small>{element.mold}</small>
+                  <br />
+                  Something else
+                </p>
               </Section>
             </Modal.Content>
-          )}
+          ))}
         </div>
       </OpenModal>
     </div>
   );
-}
+};
 
 export const WiperModal = () => {
   const { isLoading, error, data } = useQuery("wipers", getWipers);
@@ -170,24 +180,23 @@ export const WiperModal = () => {
   }
   return (
     <div className="modal-body">
-
-      { data.map( element =>
+      {data.map((element) => (
         <OpenModal modal={{ closeOnBlur: true }} name="Wiper">
           <Modal.Content>
-            <Section style={{ backgroundColor: 'white' }}>
-            <p>
-              <strong>{element.name }</strong> <small>{element.drawing}</small> <small>{element.mold}</small>
-              <br />
-              Something else
-            </p>
+            <Section style={{ backgroundColor: "white" }}>
+              <p>
+                <strong>{element.name}</strong> <small>{element.drawing}</small>{" "}
+                <small>{element.mold}</small>
+                <br />
+                Something else
+              </p>
             </Section>
           </Modal.Content>
         </OpenModal>
-      )}
-
+      ))}
     </div>
   );
-}
+};
 
 export const CapModal = () => {
   const { isLoading, error, data } = useQuery("caps", getCaps);
@@ -197,21 +206,20 @@ export const CapModal = () => {
   }
   return (
     <div className="modal-body">
-
-      { data.map( element =>
+      {data.map((element) => (
         <OpenModal modal={{ closeOnBlur: true }} name="Wiper">
           <Modal.Content>
-            <Section style={{ backgroundColor: 'white' }}>
-            <p>
-              <strong>{element.name }</strong> <small>{element.drawing}</small> <small>{element.mold}</small>
-              <br />
-              Something else
-            </p>
+            <Section style={{ backgroundColor: "white" }}>
+              <p>
+                <strong>{element.name}</strong> <small>{element.drawing}</small>{" "}
+                <small>{element.mold}</small>
+                <br />
+                Something else
+              </p>
             </Section>
           </Modal.Content>
         </OpenModal>
-      )}
-
+      ))}
     </div>
   );
-}
+};
--- routes.js
+++ routes.js
@@ -1,97 +1,108 @@
-const express = require( "express" );
+const express = require("express");
 const router = express.Router();
-const Bottle = require( "./models/bottle.model" );
-const Brush = require( "./models/brush.model" );
-const Cap = require( "./models/cap.model" );
-const Rod = require( "./models/rod.model" );
-const Wiper = require( "./models/wiper.model" );
-const Build = require( "./models/build.model" );
+const Bottle = require("./models/bottle.model");
+const Brush = require("./models/brush.model");
+const Cap = require("./models/cap.model");
+const Rod = require("./models/rod.model");
+const Wiper = require("./models/wiper.model");
+const Build = require("./models/build.model");
 
-router.get( "/", async ( req, res, next ) => {
+router.get("/", async (req, res, next) => {
   try {
-    console.log( "succesfully fetched index" );
-    return ( "welcome!");
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully fetched index");
+    return "welcome!";
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
-router.get( "/bottle", async ( req, res, next ) => {
+router.get("/bottle", async (req, res, next) => {
   try {
     const bottle = await Bottle.find();
-    console.log( "succesfully found bottles list" );
-    return res.send( bottle );
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully found bottles list");
+    return res.send(bottle);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
-router.post( "/bottle", async ( req, res, next ) => {
+router.get("/build", async (req, res, next) => {
+  try {
+    const build = await Build.find();
+    console.log("succesfully found builds list");
+    return res.send(build);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
+router.post("/bottle", async (req, res, next) => {
   const data = {
-		bottleName: req.body.bottleName,
+    bottleName: req.body.bottleName,
+  };
+
+  try {
+    const already_bottle = await Build.findOne({
+      bottleName: req.body.bottleName,
+    });
+    if (already_bottle === null) {
+      const bottle = new Build(data);
+      await bottle.save();
+      res.status(200).send("Bottle added to your build list!");
+      console.log("Bottle added to build!");
+    } else {
+      res.status(400).send("Bottle already in build list!");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
   }
-
-	try {
-		const already_bottle = await Build.findOne({ bottleName: req.body.bottleName })
-		if( already_bottle === null ) {
-			const bottle = new Build( data );
-			await bottle.save();
-      res.status( 200 ).send( "Bottle added to your build list!" );
-      console.log( "Bottle added to build!" );
-		} else {
-      res.status( 400 ).send( "Bottle already in build list!" );
-		}
-	} catch ( err ){
-    console.log( err );
-		res.status( 400 ).json( err );
-	}
-
 });
 
-router.get( "/brush", async ( req, res, next ) => {
+router.get("/brush", async (req, res, next) => {
   try {
     const brush = await Brush.find();
-    console.log( "succesfully found brushes list" );
-    return res.send( brush );
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully found brushes list");
+    return res.send(brush);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
-router.get( "/cap", async ( req, res, next ) => {
+router.get("/cap", async (req, res, next) => {
   try {
     const cap = await Cap.find();
-    console.log( "succesfully found caps list" );
-    return res.send( cap );
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully found caps list");
+    return res.send(cap);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
-router.get( "/rod", async ( req, res, next ) => {
+router.get("/rod", async (req, res, next) => {
   try {
     const rod = await Rod.find();
-    console.log( "succesfully found rods list" );
-    return res.send( rod );
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully found rods list");
+    return res.send(rod);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
-router.get( "/wiper", async ( req, res, next ) => {
+router.get("/wiper", async (req, res, next) => {
   try {
     const wiper = await Wiper.find();
-    console.log( "succesfully found wipers list" );
-    return res.send( wiper );
-  } catch ( err ) {
-    console.log( err );
-    next( err );
+    console.log("succesfully found wipers list");
+    return res.send(wiper);
+  } catch (err) {
+    console.log(err);
+    next(err);
   }
 });
 
